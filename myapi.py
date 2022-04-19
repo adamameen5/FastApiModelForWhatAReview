03a48ba66b0bbbbc5a520c66573e32e08540824e
Adam Ameen <adamameen5@gmail.com>
2022-04-20 05:10:13 +0530

    Implemented sarcasm detection - v1

--- myapi.py
+++ myapi.py
@@ -11,7 +11,27 @@ from heapq import nlargest
 import json
 from fastapi.middleware.cors import CORSMiddleware
 
-
+#for sarcasm detection
+import numpy as np
+import pandas as pd 
+import os
+import re
+import matplotlib.pyplot as plt
+import keras
+import tensorflow
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, GRU, LSTM, Bidirectional
+from keras.layers.embeddings import Embedding
+from keras.initializers import Constant
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
+import sys
+from wordcloud import WordCloud, ImageColorGenerator
 
 app = FastAPI()
 
@@ -51,6 +71,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+data_1 = pd.read_json("Sarcasm_Headlines_Dataset.json", lines=True)
+data_2 = pd.read_json("Sarcasm_Headlines_Dataset_v2.json", lines=True)
+data =  pd.concat([data_1, data_2])
 
 
 # creates an end point using the home url
@@ -158,6 +182,150 @@ def summarize(review: str):
     print(summary)
     return summary
     
+
+@app.get("/detectSarcasm/{review}")
+def detectSarcasm(review: str):
+    x_final = pd.DataFrame({"headline":[review]})
+    test_lines = CleanTokenize(x_final)
+    test_sequences = tokenizer_obj.texts_to_sequences(test_lines)
+    test_review_pad = pad_sequences(test_sequences, maxlen=max_length, padding='post')
+    pred = model.predict(test_review_pad)
+    pred*=100
+    if pred[0][0]>=50: return "It's a sarcasm!" 
+    else: return "It's not a sarcasm."
+    
+    
+def clean_text(text):
+    text = text.lower()
+    
+    pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    text = pattern.sub('', text)
+    text = " ".join(filter(lambda x:x[0]!='@', text.split()))
+    emoji = re.compile("["
+                           u"\U0001F600-\U0001FFFF"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    
+    text = emoji.sub(r'', text)
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)        
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text) 
+    text = re.sub(r"\'ll", " will", text)  
+    text = re.sub(r"\'ve", " have", text)  
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"don't", "do not", text)
+    text = re.sub(r"did't", "did not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"couldn't", "could not", text)
+    text = re.sub(r"have't", "have not", text)
+    text = re.sub(r"[,.\"\'!@#$%^&*(){}?/;`~:<>+=-]", "", text)
+    print("called clean_text")
+    return text
+
+def CleanTokenize(df):
+    head_lines = list()
+    lines = df["headline"].values.tolist()
+    print("called CleanTokenize")
+    for line in lines:
+        line = clean_text(line)
+        # tokenize the text
+        tokens = word_tokenize(line)
+        # remove puntuations
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        # remove non alphabetic characters
+        words = [word for word in stripped if word.isalpha()]
+        stop_words = set(stopwords.words("english"))
+        # remove stop words
+        words = [w for w in words if not w in stop_words]
+        head_lines.append(words)
+    return head_lines
+
+head_lines = CleanTokenize(data)
+
+validation_split = 0.2
+max_length = 25
+
+tokenizer_obj = Tokenizer()
+
+tokenizer_obj.fit_on_texts(head_lines)
+sequences = tokenizer_obj.texts_to_sequences(head_lines)
+
+word_index = tokenizer_obj.word_index
+print("unique tokens - ",len(word_index))
+vocab_size = len(tokenizer_obj.word_index) + 1
+print('vocab size -', vocab_size)
+
+lines_pad = pad_sequences(sequences, maxlen=max_length, padding='post')
+sentiment =  data['is_sarcastic'].values
+
+indices = np.arange(lines_pad.shape[0])
+np.random.shuffle(indices)
+lines_pad = lines_pad[indices]
+sentiment = sentiment[indices]
+
+num_validation_samples = int(validation_split * lines_pad.shape[0])
+
+X_train_pad = lines_pad[:-num_validation_samples]
+y_train = sentiment[:-num_validation_samples]
+X_test_pad = lines_pad[-num_validation_samples:]
+y_test = sentiment[-num_validation_samples:]
+
+print('Shape of X_train_pad:', X_train_pad.shape)
+print('Shape of y_train:', y_train.shape)
+
+print('Shape of X_test_pad:', X_test_pad.shape)
+print('Shape of y_test:', y_test.shape)
+
+embeddings_index = {}
+embedding_dim = 100
+GLOVE_DIR = '/Users/adamameen/Documents/Masters/2nd Year/Msc Project/Dataset/glove.twitter.27B'
+f = open(os.path.join(GLOVE_DIR, 'glove.twitter.27B.100d.txt'), encoding = "utf-8")
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+
+embedding_matrix = np.zeros((len(word_index) + 1, embedding_dim))
+c = 0
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        c+=1
+        embedding_matrix[i] = embedding_vector
+print(c)
+
+embedding_layer = Embedding(len(word_index) + 1,
+                        embedding_dim,
+                        weights=[embedding_matrix],
+                        input_length=max_length,
+                        trainable=False)
+model = Sequential()
+model.add(embedding_layer)
+model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.25))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+
+print('Summary of the built model...')
+print(model.summary())
 
 
 """ Original Review """
